5bda595d6fe8ad333c1ac965ce6bbe7e8b829067
Clay Morse <clarence.morse@gmail.com>
2023-04-07 09:58:17 -0400

    Comments Chapter Complete

--- web/src/components/CommentForm/CommentForm.js
+++ web/src/components/CommentForm/CommentForm.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import {
   Form,
   FormError,
@@ -7,6 +9,9 @@ import {
   Submit,
 } from '@redwoodjs/forms'
 import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
+
+import { QUERY as CommentsQuery } from 'src/components/CommentsCell'
 
 const CREATE = gql`
   mutation CreateCommentMutation($input: CreateCommentInput!) {
@@ -19,15 +24,22 @@ const CREATE = gql`
   }
 `
 
-const CommentForm = () => {
-  const [createComment, { loading, error }] = useMutation(CREATE)
+const CommentForm = ({ postId }) => {
+  const [hasPosted, setHasPosted] = useState(false)
+  const [createComment, { loading, error }] = useMutation(CREATE, {
+    onCompleted: () => {
+      setHasPosted(true)
+      toast.success('Thank you for your comment!')
+    },
+    refetchQueries: [{ query: CommentsQuery, variables: { postId } }],
+  })
 
   const onSubmit = (input) => {
-    createComment({ variables: { input } })
+    createComment({ variables: { input: { postId, ...input } } })
   }
 
   return (
-    <div>
+    <div className={hasPosted ? 'hidden' : ''}>
       <h3 className="font-light text-lg text-gray-600">Leave a Comment</h3>
       <Form className="mt-4 w-full" onSubmit={onSubmit}>
         <FormError
